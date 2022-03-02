828ca51a0004fb8870d0bee9c10427722cb0bdaa
asyildiz3363 <53121137+asyildiz3363@users.noreply.github.com>
2022-03-02 16:34:27 +0300

    Update take_data_daily.py

--- take_data_daily.py
+++ take_data_daily.py
@@ -40,8 +40,8 @@ def getdata():
         bsymbols=bsymbols1.iloc[:,0].to_list()
         for bticker in bsymbols[:10]:
             #print(index,bticker,end="\r")
+            c=s.empty
             st.write(index,bticker)
-            st.empty(index,bticker)
             index += 1
             df=yf.download(bticker,period="3mo")
             df2=df.drop('Adj Close', 1)
